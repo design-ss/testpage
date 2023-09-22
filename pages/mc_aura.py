9b5f1ee08fd51a345422137ab3fe10c6bbd18264
design-ss <135007494+design-ss@users.noreply.github.com>
2023-09-22 15:22:04 +0900

    Update mc_aura.py

--- pages/mc_aura.py
+++ pages/mc_aura.py
@@ -38,15 +38,22 @@ st.title('mcオーラ書き出し')
 
 
 # オーラ前ファイル指定
-export_files_top = st.file_uploader("オーラ前ファイルを選択", type='png', accept_multiple_files=True, key="export_files_top")
+export_files_front = st.file_uploader("オーラ前ファイルを選択", type='png', accept_multiple_files=True, key="export_files_front")
 
 # オーラ後ろファイル指定
-export_files_bottom = st.file_uploader("オーラ後ろファイルを選択", type='png', accept_multiple_files=True, key="export_files_bottom")
+export_files_back = st.file_uploader("オーラ後ろファイルを選択", type='png', accept_multiple_files=True, key="export_files_back")
 
 st.write('属性画像はローカルからアップロードお願いします。トレロに全属性画像のフォルダを記載してます。')
 # 属性ファイル
 attribution_file = st.file_uploader("属性を選択", type='png', accept_multiple_files=False, key="attribution_file")
+# ファイルが選択されていない場合はメッセージを表示する
+if not attribution_file:
+    st.write('<span style="color:red;">未選択です。属性画像をアップロードしてください。</span>', unsafe_allow_html=True)
 
+
+# ファイル名を昇順に並び替える　ローカルでは選択順にアップされるが、クラウド上ではなぜかバラバラになるので制御するために昇順に
+export_files_front = sorted(export_files_front, key=lambda x: x.name)
+export_files_back = sorted(export_files_back, key=lambda x: x.name)
 
 # パターン1説明
 st.write('100/50の見た目の中心を取って配置します。')
@@ -68,33 +75,35 @@ with export_button1:
         with st.spinner("画像生成中です..."):
             binary_dict.clear() # 初期化
              # 前ファイルと後ろファイル　
-            if not export_files_top:
-                export_files_top = [None] * len(export_files_bottom)
-            if not export_files_bottom:
-                export_files_bottom = [None] * len(export_files_top)
-            export_files = list(zip(export_files_top, export_files_bottom))
+            if not export_files_front:
+                export_files_front = [None] * len(export_files_back)
+            if not export_files_back:
+                export_files_back = [None] * len(export_files_front)
+            export_files = list(zip(export_files_front, export_files_back))
             
-            for export_file_top, export_file_bottom in export_files:
+            for export_file_front, export_file_back in export_files:
                 ####################################
 
                 #　50 × 50、100×100　のリサイズ
 
                 ####################################
                 # 画像を読み込む
-                if export_file_top:
-                    image_top = Image.open(export_file_top)
+                if export_file_front:
+                    image_front = Image.open(export_file_front)
+                    image_front = image_front.resize((960, 640))
                 else:
-                    image_top = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_front = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-                if export_file_bottom:
-                    image_bottom = Image.open(export_file_bottom)
+                if export_file_back:
+                    image_back = Image.open(export_file_back)
+                    image_back = image_back.resize((960, 640))
                 else:
-                    image_bottom = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_back = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
                 attribution = Image.open(attribution_file)
 
                 # 統合する
-                image = Image.alpha_composite(image_bottom, image_top)
+                image = Image.alpha_composite(image_back, image_front)
 
                 # 不要な透明画素を除去
                 image = image.crop(image.getbbox())
@@ -113,19 +122,19 @@ with export_button1:
 
                 # 画像を中央に合わせて切り抜く
                 left = (resized_image.width - 100) // 2
-                top = (resized_image.height - 100) // 2
+                front = (resized_image.height - 100) // 2
                 right = left + 100
-                bottom = top + 100
-                b_image = resized_image.crop((left, top, right, bottom))
+                back = front + 100
+                b_image = resized_image.crop((left, front, right, back))
 
                 # 統合する
                 b_image = Image.alpha_composite(b_image, attribution)
 
                  # ファイル名を設定する
-                if export_file_top:
-                    file_name = export_file_top.name
+                if export_file_front:
+                    file_name = export_file_front.name
                 else:
-                    file_name = export_file_bottom.name
+                    file_name = export_file_back.name
 
                 # 100 × 100保存
                 binary_dict["/100x100/" + file_name] = b_image
@@ -141,30 +150,33 @@ with export_button1:
 
                 ####################################
 
-               # 画像を読み込む
-                if export_file_top:
-                    image_top = Image.open(export_file_top)
+                # 画像を読み込む
+                if export_file_front:
+                    image_front = Image.open(export_file_front)
+                    image_front = image_front.resize((960, 640))
                 else:
-                    image_top = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_front = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-                if export_file_bottom:
-                    image_bottom = Image.open(export_file_bottom)
+                if export_file_back:
+                    image_back = Image.open(export_file_back)
+                    image_back = image_back.resize((960, 640))
                 else:
-                    image_bottom = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_back = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    
 
                 attribution = Image.open(attribution_file)
                 
                 # 960×640
-                if export_file_top:
-                    image_top = image_top.resize((960, 640))
-                    binary_dict["/960x640/" + export_file_top.name] = image_top
-                if export_file_bottom:
-                    image_bottom = image_bottom.resize((960, 640))
-                    binary_dict["/960x640/" + export_file_bottom.name] = image_bottom
+                if export_file_front:
+                    image_front = image_front.resize((960, 640))
+                    binary_dict["/960x640/" + export_file_front.name] = image_front
+                if export_file_back:
+                    image_back = image_back.resize((960, 640))
+                    binary_dict["/960x640/" + export_file_back.name] = image_back
 
 
                 # 統合する
-                image = Image.alpha_composite(image_bottom, image_top)
+                image = Image.alpha_composite(image_back, image_front)
 
                 # 不要な透明画素を除去
                 image = image.crop(image.getbbox())
@@ -182,19 +194,19 @@ with export_button1:
 
                 # 画像を下に移動
                 left = (resized_image.width - 640) / 2
-                top = resized_image.height - 640 * 0.85
+                front = resized_image.height - 640 * 0.85
                 right = left + 640
-                bottom = top + 640
-                d_image = resized_image.crop((left, top, right, bottom))
+                back = front + 640
+                d_image = resized_image.crop((left, front, right, back))
                 
                 # 320×320を生成
                 c_image = d_image.resize((320, 320))
                 
                  # ファイル名を設定する
-                if export_file_top:
-                    file_name = export_file_top.name
+                if export_file_front:
+                    file_name = export_file_front.name
                 else:
-                    file_name = export_file_bottom.name
+                    file_name = export_file_back.name
 
                 # 統合した画像の保存（
                 binary_dict["/640x640/" + file_name] = d_image
@@ -213,37 +225,42 @@ st.markdown('---')
 with st.spinner("プレビュー画像生成中です..."):
     binary_dict.clear() # 初期化
      # 前ファイルと後ろファイルを結合
-    if not export_files_top:
-        export_files_top = [None] * len(export_files_bottom)
-    if not export_files_bottom:
-        export_files_bottom = [None] * len(export_files_top)
-    export_files = list(zip(export_files_top, export_files_bottom))
+    if not export_files_front:
+        export_files_front = [None] * len(export_files_back)
+    if not export_files_back:
+        export_files_back = [None] * len(export_files_front)
+    export_files = list(zip(export_files_front, export_files_back))
 
     # 全部プレビュー　enumerate関数リスト型変数から要素を一つずつ取り出し、要素の位置と要素をタプル型変数として返す
     # プレビュー画像にチェックボックスを付ける　個別書き出し用の空のリスト作る
     selected_files = []
     cols = st.columns(4)
-    for i, (export_file_top, export_file_bottom) in enumerate(export_files):
+    for i, (export_file_front, export_file_back) in enumerate(export_files):
         ####################################
 
         #　50 × 50、100×100　のリサイズ
 
         ####################################
         # 画像を読み込む
-        if export_file_top:
-            image_top = Image.open(export_file_top)
+        if export_file_front:
+            image_front = Image.open(export_file_front)
+            image_front = image_front.resize((960, 640))
         else:
-            image_top = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+            image_front = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-        if export_file_bottom:
-            image_bottom = Image.open(export_file_bottom)
+        if export_file_back:
+            image_back = Image.open(export_file_back)
+            image_back = image_back.resize((960, 640))
         else:
-            image_bottom = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+            image_back = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-        attribution = Image.open(attribution_file)
-
+        try:
+            attribution = Image.open(attribution_file)
+        except AttributeError:
+            st.error('属性ファイルをアップしてください。')
+            
         # 統合する
-        image = Image.alpha_composite(image_bottom, image_top)
+        image = Image.alpha_composite(image_back, image_front)
 
         # 不要な透明画素を除去
         image = image.crop(image.getbbox())
@@ -262,19 +279,19 @@ with st.spinner("プレビュー画像生成中です..."):
 
         # 画像を中央に合わせて切り抜く
         left = (resized_image.width - 100) // 2
-        top = (resized_image.height - 100) // 2
+        front = (resized_image.height - 100) // 2
         right = left + 100
-        bottom = top + 100
-        b_image = resized_image.crop((left, top, right, bottom))
+        back = front + 100
+        b_image = resized_image.crop((left, front, right, back))
 
         # 統合する
         b_image = Image.alpha_composite(b_image, attribution)
         
         # ファイル名を設定する
-        if export_file_top:
-            file_name = export_file_top.name
+        if export_file_front:
+            file_name = export_file_front.name
         else:
-            file_name = export_file_bottom.name
+            file_name = export_file_back.name
 
         # 中心線を描画する
         draw = ImageDraw.Draw(b_image)
@@ -289,7 +306,7 @@ with st.spinner("プレビュー画像生成中です..."):
         # if len(file_name) > 6:
         #     file_name = file_name[:6] + "..."
         # if cols[i % 4].checkbox(file_name, key=f"select_{file_name}"):
-        #     selected_files.append((export_file_top, export_file_bottom))
+        #     selected_files.append((export_file_front, export_file_back))
 
 
 
@@ -299,33 +316,35 @@ with export_selected_button1:
         with st.spinner("画像生成中です..."):
             binary_dict.clear() # 初期化
              # 前ファイルと後ろファイルを結合
-            if not export_files_top:
-                export_files_top = [None] * len(export_files_bottom)
-            if not export_files_bottom:
-                export_files_bottom = [None] * len(export_files_top)
-            export_files = list(zip(export_files_top, export_files_bottom))
+            if not export_files_front:
+                export_files_front = [None] * len(export_files_back)
+            if not export_files_back:
+                export_files_back = [None] * len(export_files_front)
+            export_files = list(zip(export_files_front, export_files_back))
 
-            for export_file_top, export_file_bottom in selected_files:
+            for export_file_front, export_file_back in selected_files:
                 ####################################
 
                 #　50 × 50、100×100　のリサイズ
 
                 ####################################
                 # 画像を読み込む
-                if export_file_top:
-                    image_top = Image.open(export_file_top)
+                if export_file_front:
+                    image_front = Image.open(export_file_front)
+                    image_front = image_front.resize((960, 640))
                 else:
-                    image_top = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_front = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-                if export_file_bottom:
-                    image_bottom = Image.open(export_file_bottom)
+                if export_file_back:
+                    image_back = Image.open(export_file_back)
+                    image_back = image_back.resize((960, 640))
                 else:
-                    image_bottom = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_back = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
                 attribution = Image.open(attribution_file)
 
                 # 統合する
-                image = Image.alpha_composite(image_bottom, image_top)
+                image = Image.alpha_composite(image_back, image_front)
 
                 # 不要な透明画素を除去
                 image = image.crop(image.getbbox())
@@ -344,19 +363,19 @@ with export_selected_button1:
 
                 # 画像を中央に合わせて切り抜く
                 left = (resized_image.width - 100) // 2
-                top = (resized_image.height - 100) // 2
+                front = (resized_image.height - 100) // 2
                 right = left + 100
-                bottom = top + 100
-                b_image = resized_image.crop((left, top, right, bottom))
+                back = front + 100
+                b_image = resized_image.crop((left, front, right, back))
 
                 # 統合する
                 b_image = Image.alpha_composite(b_image, attribution)
 
                  # ファイル名を設定する
-                if export_file_top:
-                    file_name = export_file_top.name
+                if export_file_front:
+                    file_name = export_file_front.name
                 else:
-                    file_name = export_file_bottom.name
+                    file_name = export_file_back.name
 
                 # 100 × 100保存
                 binary_dict["/100x100/" + file_name] = b_image
@@ -371,31 +390,32 @@ with export_selected_button1:
                 #　640 × 640、320 ×　320　のリサイズ
 
                 ####################################
-
                # 画像を読み込む
-                if export_file_top:
-                    image_top = Image.open(export_file_top)
+                if export_file_front:
+                    image_front = Image.open(export_file_front)
+                    image_front = image_front.resize((960, 640))
                 else:
-                    image_top = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_front = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
-                if export_file_bottom:
-                    image_bottom = Image.open(export_file_bottom)
+                if export_file_back:
+                    image_back = Image.open(export_file_back)
+                    image_back = image_back.resize((960, 640))
                 else:
-                    image_bottom = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
+                    image_back = Image.new("RGBA", (960, 640), (0, 0, 0, 0))
 
                 attribution = Image.open(attribution_file)
                 
                 # 960×640
-                if export_file_top:
-                    image_top = image_top.resize((960, 640))
-                    binary_dict["/960x640/" + export_file_top.name] = image_top
-                if export_file_bottom:
-                    image_bottom = image_bottom.resize((960, 640))
-                    binary_dict["/960x640/" + export_file_bottom.name] = image_bottom
+                if export_file_front:
+                    image_front = image_front.resize((960, 640))
+                    binary_dict["/960x640/" + export_file_front.name] = image_front
+                if export_file_back:
+                    image_back = image_back.resize((960, 640))
+                    binary_dict["/960x640/" + export_file_back.name] = image_back
 
 
                 # 統合する
-                image = Image.alpha_composite(image_bottom, image_top)
+                image = Image.alpha_composite(image_back, image_front)
 
                 # 不要な透明画素を除去
                 image = image.crop(image.getbbox())
@@ -413,19 +433,19 @@ with export_selected_button1:
 
                 # 画像を下に移動
                 left = (resized_image.width - 640) / 2
-                top = resized_image.height - 640 * 0.85
+                front = resized_image.height - 640 * 0.85
                 right = left + 640
-                bottom = top + 640
-                d_image = resized_image.crop((left, top, right, bottom))
+                back = front + 640
+                d_image = resized_image.crop((left, front, right, back))
                 
                 # 320×320を生成
                 c_image = d_image.resize((320, 320))
                 
                  # ファイル名を設定する
-                if export_file_top:
-                    file_name = export_file_top.name
+                if export_file_front:
+                    file_name = export_file_front.name
                 else:
-                    file_name = export_file_bottom.name
+                    file_name = export_file_back.name
 
                 # 統合した画像の保存（
                 binary_dict["/640x640/" + file_name] = d_image
